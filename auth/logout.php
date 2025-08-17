1256520b7c224cacbda70ba08068029afaaec4fb
Kibetinho <drewkibet@gmail.com>
2025-08-17 21:01:29 +0300

    logout

---
+++ auth/logout.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../config/connection.php';
+require_once __DIR__ . '/../services/Auth.php';
+
+Auth::logout();
+header('Location: ' . ROOT_URL . '/index.php');
+exit;
+
+
