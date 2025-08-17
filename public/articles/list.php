38136ffda3ea941317882332d8141306989e4e3d
Kibetinho <drewkibet@gmail.com>
2025-08-17 23:03:18 +0300

    correcting and adding codes

--- public/articles/list.php
+++ public/articles/list.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../config/connection.php';
+require_once('config/connection.php');
 require_once __DIR__ . '/../../services/Auth.php';
 require_once __DIR__ . '/../../models/Article.php';
 require_once __DIR__ . '/../../services/Security.php';
